95ad65fc3f32a69c9789bc1efb33fdf3ac52d5a6
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-28 20:15:54 -0400

    create database schemas and copy csv data into databases

--- database/Products/schema.sql
+++ database/Products/schema.sql
@@ -6,59 +6,110 @@ CREATE DATABASE productsapi;
 
 CREATE TABLE products (
   id serial PRIMARY KEY,
-  product_id int NOT NULL unique,
-  name VARCHAR(255) NOT NULL unique,
-  slogan TEXT NOT NULL,
-  description TEXT NOT NULL,
+  name VARCHAR(255),
+  slogan TEXT,
+  description TEXT,
   category VARCHAR (255),
-  default_price int NOT NULL
+  default_price int
 );
 
 CREATE TABLE features (
-  feature_id serial PRIMARY KEY,
+  id serial PRIMARY KEY,
   product_id int,
   CONSTRAINT ft_productId
     FOREIGN KEY (product_id)
-      REFERENCES products(product_id),
-  feature VARCHAR(255) NOT NULL,
-  value VARCHAR(255) NOT NULL
+      REFERENCES products(id),
+  feature VARCHAR(255),
+  value VARCHAR(255)
 );
 
 CREATE TABLE styles (
-  style_id serial PRIMARY KEY,
-  product_id int,
+  id serial PRIMARY KEY,
+  productId int,
   CONSTRAINT st_productId
-    FOREIGN KEY (product_id)
-      REFERENCES products(product_id),
-  name VARCHAR(255) NOT NULL unique,
-  sale_price int,
-  original_price int NOT NULL,
-  default_style int NOT NULL
+    FOREIGN KEY (productId)
+      REFERENCES products(id),
+  name VARCHAR(255),
+  sale_price text,
+  original_price int,
+  default_style int
+);
+
+CREATE TABLE skus (
+  id serial PRIMARY KEY,
+  styleId int,
+  CONSTRAINT sk_styleId
+    FOREIGN KEY (styleId)
+      REFERENCES styles(id),
+  size VARCHAR(255),
+  quantity int
 );
 
 CREATE TABLE photos (
   id serial PRIMARY KEY,
-  style_id int unique,
+  styleId int,
   CONSTRAINT st_styleId
-    FOREIGN KEY (style_id)
-      REFERENCES styles(style_id),
-  url TEXT NOT NULL,
-  thumbnail_url TEXT NOT NULL
+    FOREIGN KEY (styleId)
+      REFERENCES styles(id),
+  url TEXT,
+  thumbnail_url TEXT
 );
 
 CREATE TABLE related (
   id serial PRIMARY KEY,
-  current_product_id int NOT NULL,
-  related_product_id int NOT NULL
+  current_product_id int,
+  related_product_id int
 );
 
 CREATE Table cart (
   id serial PRIMARY KEY,
+  user_session int,
   product_id int,
   CONSTRAINT ct_productId
     FOREIGN KEY (product_id)
-      REFERENCES products(product_id),
-  user_session int NOT NULL,
-  active int NOT NULL
+      REFERENCES products(id),
+  active int
 );
 
+
+-- Copy products csv data
+COPY products(id,name,slogan,description,category,default_price)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/product.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy features csv data
+COPY features(id,product_id,feature,value)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/features.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy styles csv data
+COPY styles(id,productId,name,sale_price,original_price,default_style)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/styles.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy skus csv data
+COPY skus(id,styleId,size,quantity)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/skus.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy photos csv data
+COPY photos(id,styleId,url,thumbnail_url)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy related csv data
+COPY related(id,current_product_id,related_product_id)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/related.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy cart csv data
+COPY cart(id,user_session,product_id,active)
+FROM '/Users/sebastian/Desktop/System Design Capstone/SDC/database/Products/csv/cart.csv'
+DELIMITER ','
+CSV HEADER;
