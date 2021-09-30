3f79188666dc716685cd7d47b1ad11fedd1e65ed
Ye Feng Chen <yefengchen96@gmail.com>
2021-09-29 20:11:02 -0400

    Day 3 Complete, able to get the data and format similar to FEC API

--- server/controllers/QuestionsAndAnswers.js
+++ server/controllers/QuestionsAndAnswers.js
@@ -2,7 +2,21 @@ const models = require('../models').questionsAndAnswers;
 
 module.exports = {
   get: (req, res) => {
-    models.get((err, data) => {
+    models.get(req.query.product_id, (err, data) => {
+      if (err) {
+        res.status(404).end();
+      } else {
+        let result = {
+          product_id: req.query.product_id,
+          results: data
+        }
+        res.send(result);
+      }
+    })
+  },
+
+  post: (req, res) => {
+    models.post((err, data) => {
       if (err) {
         res.status(418).end();
       } else {
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ let controller = require('./controllers');
 let router = require('express').Router();
 
 router.get('/qa', controller.questionsAndAnswers.get);
+router.post('/qa', controller.questionsAndAnswers.post);
 
 // Products API Endpoints
 router.get('/products', controller.Products.get);
