70ae026a2d888131e991f1f1ccfd668893b8e53d
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-01 14:32:29 -0400

    POST and PUT requests are complete

--- server/models/QuestionsAndAnswers.js
+++ server/models/QuestionsAndAnswers.js
@@ -1,43 +1,7 @@
 const db = require('../../database').questionsAndAnswers;
 
-const formatData = (result) => {
-  let dataObj = {};
-  for (let i = 0; i < result.rows.length; i++) {
-    let currentRow = result.rows[i];
-    if (dataObj[currentRow.question_id] === undefined) {
-      dataObj[currentRow.question_id] = {
-        question_id: currentRow.question_id,
-        question_body: currentRow.question_body,
-        question_date: new Date(Number(currentRow.question_date)),
-        asker_name: currentRow.asker_name,
-        question_helpfulness: currentRow.question_helpfulness,
-        reported: currentRow.reported === 0 ? false : true,
-        answers: {}
-      }
-    }
-    if (currentRow.answer_id !== null) {
-      dataObj[currentRow.question_id].answers[currentRow.answer_id] = {
-        id: currentRow.answer_id,
-        body: currentRow.answer_body,
-        date: new Date(Number(currentRow.answer_date)),
-        answerer_name: currentRow.answerer_name,
-        helpfulness: currentRow.answer_helpfulness,
-        photos: []
-      }
-    }
-    if (currentRow.url !== null) {
-      dataObj[currentRow.question_id].answers[currentRow.answer_id].photos.push(currentRow.url);
-    }
-  };
-  let data = [];
-  for (let key in dataObj) {
-    data.push(dataObj[key]);
-  }
-  return data;
-}
-
 module.exports = {
-  get: (product_id, callback) => {
+  getQuestions: (product_id) => {
     let queryString =
       `SELECT
         questions.id AS question_id,
@@ -63,13 +27,96 @@ module.exports = {
       ON
         (answers.id = answers_photos.answer_id)
       WHERE
-        questions.product_id = ($1)`;
-    db.query(queryString, [product_id], (err, result) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, formatData(result));
-      }
-    })
+        questions.product_id = $1 AND questions.reported = 0
+      ORDER BY
+        questions.id ASC`;
+    return db.query(queryString, [product_id]);
+  },
+
+  getAnswers: (question_id, count, page) => {
+    let queryString =
+      `SELECT
+        answers.id AS answer_id,
+        answers.body AS answer_body,
+        answers.date_written AS answer_date,
+        answers.answerer_name,
+        answers.helpful AS answer_helpfulness,
+        answers_photos.id AS photo_id,
+        answers_photos.url
+      FROM
+        answers
+      LEFT JOIN
+        answers_photos
+      ON
+        (answers.id = answers_photos.answer_id)
+      WHERE
+        answers.question_id = $1 AND answers.reported = 0
+      ORDER BY
+        answers.id ASC`;
+    return db.query(queryString, [question_id]);
+  },
+
+  postQuestion: (postInfo) => {
+    let queryString = 'SELECT MAX(id) FROM questions';
+    return db.query(queryString)
+      .then(id => {
+        queryString =
+          `INSERT INTO
+            questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+          VALUES
+            ($1, $2, $3, $4, $5, $6, $7, $8)
+          RETURNING id`;
+        return db.query(queryString, [id.rows[0].max + 1, ...postInfo]);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+
+  postAnswer: (postInfo) => {
+    let queryString = 'SELECT MAX(id) FROM answers';
+    return db.query(queryString)
+      .then(id => {
+        queryString =
+        `INSERT INTO
+          answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
+        VALUES
+          ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING id`;
+        return db.query(queryString,[id.rows[0].max + 1, ... postInfo]);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+
+  postPhotos: (answer_id, photos) => {
+    let queryString = 'SELECT MAX(id) FROM answers_photos';
+    return db.query(queryString)
+      .then(id => {
+        let intoPhotos = [];
+        queryString =
+        `INSERT INTO
+          answers_photos(id, answer_id, url)
+        VALUES
+          ($1, $2, $3)`;
+        for (let i = 0; i < photos.length; i++) {
+          intoPhotos.push(db.query(queryString, [(id.rows[0].max + i + 1), answer_id, photos[i]]));
+        }
+        return Promise.all(intoPhotos);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+
+  updateHelpful: (table, id) => {
+    let queryString = `UPDATE ${table} SET helpful = helpful + 1 WHERE id = $1`;
+    return db.query(queryString, [id]);
+  },
+
+  updateReport: (table, id) => {
+    let queryString = `UPDATE ${table} SET reported = 1 WHERE id = $1`;
+    return db.query(queryString, [id]);
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,14 @@
 let controller = require('./controllers');
 let router = require('express').Router();
 
-router.get('/qa', controller.questionsAndAnswers.get);
-router.post('/qa', controller.questionsAndAnswers.post);
+router.get('/qa/questions', controller.questionsAndAnswers.getQuestions);
+router.get('/qa/questions/:question_id/answers', controller.questionsAndAnswers.getAnswers);
+router.post('/qa/questions', controller.questionsAndAnswers.postQuestion);
+router.post('/qa/questions/:question_id/answers', controller.questionsAndAnswers.postAnswer);
+router.put('/qa/questions/:question_id/helpful', controller.questionsAndAnswers.updateQuestionHelpful);
+router.put('/qa/questions/:question_id/report', controller.questionsAndAnswers.updateQuestionReport);
+router.put('/qa/answers/:answer_id/helpful', controller.questionsAndAnswers.updateAnswerHelpful);
+router.put('/qa/answers/:answer_id/report', controller.questionsAndAnswers.updateAnswerReport);
 
 // Products API Endpoints
 router.get('/products', controller.Products.get);
