2ceed938f2db1b10a30320646e488626f7ac3ca5
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-10-01 14:00:15 -0400

    add routes to server

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,11 @@ let router = require('express').Router();
 router.get('/qa', controller.QuestionsAndAnswers.get);
 
 // Products API Endpoints
-router.get('/products', controller.Products.get);
+router.get('/products', controller.Products.getProducts);
+router.get('/products/:product_id', controller.Products.getProductData);
+router.get('/products/:product_id/styles', controller.Products.getProductStyle);
+router.get('/products/:product_id/related', controller.Products.getRelated);
+router.get('/cart', controller.Products.getCart);
 
 router.get('/reviews', controller.reviews.getReviews);
 
